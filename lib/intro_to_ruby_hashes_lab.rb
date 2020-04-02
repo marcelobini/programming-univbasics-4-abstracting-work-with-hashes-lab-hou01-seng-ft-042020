defce52d806edcc738db7820fd88708b358c8e2d
Marcelo Bini <marcelobini@gmail.com>
2020-04-02 19:41:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash_creator = {:name=>"Grace Hopper"} 
+  hash_hashing = {
+    key => value
+  } 
 end
 
 def read_from_hash(hash, key)
